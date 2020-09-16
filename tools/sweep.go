f4c6ab04c012235e2b6fd9d9c7567cdd49159460
Sander Blue <sblue@newrelic.com>
2020-09-16 16:05:50 -0500

    fix(policies): get policy delete to work again

--- tools/sweep.go
+++ tools/sweep.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/newrelic/newrelic-client-go/newrelic"
 	"github.com/newrelic/newrelic-client-go/pkg/alerts"
@@ -13,7 +14,7 @@ import (
 	"github.com/newrelic/newrelic-client-go/pkg/region"
 )
 
-func newClient() *newrelic.NewRelic {
+func newClient() (*newrelic.NewRelic, *newrelic.NewRelic) {
 	client, err := newrelic.New(
 		newrelic.ConfigPersonalAPIKey(os.Getenv("NEW_RELIC_API_KEY")),
 		newrelic.ConfigAdminAPIKey(os.Getenv("NEW_RELIC_ADMIN_API_KEY")),
@@ -22,31 +23,44 @@ func newClient() *newrelic.NewRelic {
 		newrelic.ConfigRegion(region.Name("US")),
 	)
 
+	restClient, _ := newrelic.New(
+		// newrelic.ConfigPersonalAPIKey(os.Getenv("NEW_RELIC_API_KEY")),
+		newrelic.ConfigAdminAPIKey(os.Getenv("NEW_RELIC_ADMIN_API_KEY")),
+		newrelic.ConfigUserAgent("newrelic/newrelic-client-go (automated account sweeper)"),
+		newrelic.ConfigServiceName("integration-test-account-sweeper"),
+		newrelic.ConfigRegion(region.Name("US")),
+	)
+
 	if err != nil {
 		log.Fatalf("failed to initialize client with error: %+v", err)
-		return nil
+		return nil, nil
 	}
 
-	return client
+	return client, restClient
 }
 
 func main() {
-	client := newClient()
+	client, _ := newClient()
 
-	if err := deleteApplicationEntities(client); err != nil {
-		log.Println(err)
-	}
+	// if err := deleteApplicationEntities(client, restClient); err != nil {
+	// 	log.Println(err)
+	// }
 
 	if err := deletePolicies(client); err != nil {
 		log.Println(err)
 	}
 }
 
-func deleteApplicationEntities(client *newrelic.NewRelic) error {
+func deleteApplicationEntities(client *newrelic.NewRelic, restClient *newrelic.NewRelic) error {
 	applicationEntities, err := client.Entities.SearchEntities(entities.SearchEntitiesParams{
 		Domain: entities.EntityDomains.APM,
 		Name:   "tf_test",
 	})
+
+	fmt.Print("\n\n **************************** \n")
+	fmt.Printf("\n Found applicationEntities:  %+v \n", len(applicationEntities))
+	fmt.Print("\n **************************** \n\n")
+	time.Sleep(3 * time.Second)
 
 	if err != nil {
 		return fmt.Errorf("ERROR: %+v", err)
@@ -62,7 +76,16 @@ func deleteApplicationEntities(client *newrelic.NewRelic) error {
 	deleted := []int{}
 
 	for _, e := range applicationEntities {
+		// fmt.Printf("\n ID:  %+v \n", *e.ApplicationID)
+
 		if e.ApplicationID != nil {
+
+			// if _, err := client.APM.DeleteApplication(*e.ApplicationID); err != nil {
+			// 	log.Printf("[WARN] Error deleting application ID %v with name %v: %v. Continuing to next entity...", *e.ApplicationID, e.Name, err)
+			// } else {
+			// 	deleted = append(deleted, *e.ApplicationID)
+			// }
+
 			// async
 			go func(id int) {
 				if _, err := client.APM.DeleteApplication(*e.ApplicationID); err != nil {
@@ -75,6 +98,8 @@ func deleteApplicationEntities(client *newrelic.NewRelic) error {
 			}(*e.ApplicationID)
 		}
 	}
+
+	log.Printf("deleted %d applications", len(deleted))
 
 	for {
 		// do some stuff
@@ -109,25 +134,44 @@ func deletePolicies(client *newrelic.NewRelic) error {
 		return nil
 	}
 
-	ch := make(chan string, len(policies))
-	deleted := []string{}
-
-	for _, e := range policies {
-		if isIntegrationTestResource(e.Name) {
-			// async
-			go func(id string) {
-				if _, err := client.Alerts.DeletePolicyMutation(accountID, id); err != nil {
-					log.Printf("[WARN] Error deleting policy %v: %v. Continuing to next policy...", id, err)
-				} else {
-					deleted = append(deleted, id)
-				}
-
-				ch <- id
-			}(e.ID)
+	testPoliciesToDelete := []*alerts.AlertsPolicy{}
+	for _, p := range policies {
+		if isIntegrationTestResource(p.Name) {
+			testPoliciesToDelete = append(testPoliciesToDelete, p)
 		}
 	}
 
-	log.Printf("deleted %d policies", len(deleted))
+	ch := make(chan string, len(testPoliciesToDelete))
+	deleted := []string{}
+	responses := []string{}
+
+	for _, policy := range testPoliciesToDelete {
+		// async
+		go func(id string) {
+			if _, err := client.Alerts.DeletePolicyMutation(accountID, id); err != nil {
+				log.Printf("[WARN] Error deleting policy %v: %v. Continuing to next policy...", id, err)
+			} else {
+				log.Printf("[Info] Deleted policy: %v", id)
+				deleted = append(deleted, id)
+
+				ch <- id
+			}
+		}(policy.ID)
+	}
+
+	for {
+		// do some stuff
+		r, ok := <-ch
+		if !ok {
+			break
+		}
+
+		responses = append(responses, r)
+		if len(responses) == len(deleted) {
+			log.Printf("deleted %d policies", len(deleted))
+			close(ch)
+		}
+	}
 
 	return nil
 }
@@ -138,6 +182,7 @@ func isIntegrationTestResource(name string) bool {
 		"tf_test",
 		"k8s-test",
 		"test-integration",
+		"test-",
 	}
 
 	for _, testName := range testNames {
